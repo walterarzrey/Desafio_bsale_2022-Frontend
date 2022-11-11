9e55c30579742f00569110c3e89c68892dbf4b27
unknown <walterarzapalo23@gmail.com>
2022-11-11 01:30:34 -0500

    Implementar paginación mediante comprobaciones para páginas contiguas a la actual, inicio y final

--- product.js
+++ product.js
@@ -3,6 +3,10 @@ const API_URL = 'http://localhost:5000';
 
 // Clase Producto
 class Product {
+    /**
+     * @param {string} product_name - Nombre del producto a buscar
+     * @param {string} page - Número de página a listar
+     */
 
     // Limpia la página
     clearPage() {
@@ -11,14 +15,20 @@ class Product {
     }
 
     // Lista todos los productos
-    loadProducts() {
+    loadProducts(page) {
         this.clearPage();
+
+        // Variables de comprobación de métodos (paginación)
+        if (!page) {
+            page = 1;
+        }
+
+        // Variables de comprobación de métodos (paginación)
+        const product_name = '';
+        const category = '';
+
         // Elemento principal (main)
         const HTMLResponse = document.querySelector('#product');
-
-        // Div principal
-        const main_div = document.createElement('div');
-        main_div.classList = 'grid';
 
         // Formato de moneda
         const formatter = new Intl.NumberFormat('en-US', {
@@ -27,7 +37,7 @@ class Product {
         });
 
         // Método que consume la API
-        fetch(`${API_URL}/products`, {
+        fetch(`${API_URL}/products?page=${page}`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
@@ -40,6 +50,7 @@ class Product {
                 return response.json()
             })
             .then(products => {
+                this.pagination(products.totalProducts, products.ITEMS_PER_PAGE, page, product_name, category);
                 products.products.forEach((product) => {
                     // Nombre del producto
                     let name = document.createElement('h1');
@@ -75,6 +86,9 @@ class Product {
                     let article = document.createElement('article');
                     article.classList = 'mycard product-item';
 
+                    // Paginación
+
+
                     image_container.appendChild(img);
                     article.appendChild(image_container);
                     header.appendChild(name);
@@ -83,8 +97,7 @@ class Product {
                     content.appendChild(price);
                     article.appendChild(content);
 
-                    main_div.appendChild(article);
-                    HTMLResponse.appendChild(main_div);
+                    HTMLResponse.appendChild(article);
                 });
             })
             .catch(error => {
@@ -93,21 +106,26 @@ class Product {
     }
 
     // Lista los productos buscados por nombre
-    loadProductsByName(product_name) {
+    loadProductsByName(product_name, page) {
         this.clearPage();
+
+        // Comporbación de página inicial
+        if (!page) {
+            page = 1;
+        }
+
+        // Variable de comprobación de métodos (paginación)
+        const category = '';
+
         // Elemento principal (main)
         const HTMLResponse = document.querySelector('#product');
-
-        // Div principal
-        const main_div = document.createElement('div');
-        main_div.classList = 'grid';
 
         // Formato de moneda
         const formatter = new Intl.NumberFormat('en-US', {
             style: 'currency',
             currency: 'USD',
         });
-        fetch(`${API_URL}/products?product_name=${product_name}`, {
+        fetch(`${API_URL}/products?product_name=${product_name}&page=${page}`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
@@ -120,6 +138,7 @@ class Product {
                 return response.json()
             })
             .then(products => {
+                this.pagination(products.totalProducts, products.ITEMS_PER_PAGE, page, product_name, category);
                 products.products.forEach((product) => {
                     // Nombre del producto
                     let name = document.createElement('h1');
@@ -163,8 +182,7 @@ class Product {
                     content.appendChild(price);
                     article.appendChild(content);
 
-                    main_div.appendChild(article);
-                    HTMLResponse.appendChild(main_div);
+                    HTMLResponse.appendChild(article);
                 });
             })
             .catch(error => {
@@ -173,21 +191,26 @@ class Product {
     }
 
     // Lista los productos por categoría
-    loadProductsByCategory(category) {
+    loadProductsByCategory(category, page) {
         this.clearPage();
+
+        // Comporbación de página inicial
+        if (!page) {
+            page = 1;
+        }
+
+        // Variables de comprobación de métodos (paginación)
+        const product_name = '';
+
         // Elemento principal (main)
         const HTMLResponse = document.querySelector('#product');
-
-        // Div principal
-        const main_div = document.createElement('div');
-        main_div.classList = 'grid';
 
         // Formato de moneda
         const formatter = new Intl.NumberFormat('en-US', {
             style: 'currency',
             currency: 'USD',
         });
-        fetch(`${API_URL}/products/` + category, {
+        fetch(`${API_URL}/products/` + category+`?page=${page}`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
@@ -200,6 +223,7 @@ class Product {
                 return response.json()
             })
             .then(products => {
+                this.pagination(products.totalProducts, products.ITEMS_PER_PAGE, page, product_name, category);
                 products.products.forEach((product) => {
                     // Nombre del producto
                     let name = document.createElement('h1');
@@ -243,13 +267,118 @@ class Product {
                     content.appendChild(price);
                     article.appendChild(content);
 
-                    main_div.appendChild(article);
-                    HTMLResponse.appendChild(main_div);
+                    HTMLResponse.appendChild(article);
                 });
             })
             .catch(error => {
                 console.log(error);
             })
+    }
+
+    pagination(totalProducts, ITEMS_PER_PAGE, page, product_name, category) {
+        // Elemento principal - paginación
+        const paginate = document.querySelector('#pagination');
+
+        // Limpiar paginación
+        paginate.innerHTML = '';
+        
+        // variables para las páginas
+        const currentPage = page;
+        const hasNextPage = ITEMS_PER_PAGE * page < totalProducts;
+        const hasPreviousPage = page > 1;
+        const nextPage = page + 1;
+        const previousPage = page - 1;
+        const lastPage = Math.ceil(totalProducts / ITEMS_PER_PAGE);
+
+        // Vista de paginación
+        if (currentPage === 1) {
+            let a = document.createElement('a');
+            a.innerHTML = '1';
+            a.classList = 'active';
+            paginate.appendChild(a);
+        }
+        if (currentPage !== 1 && previousPage !== 1) {
+            let a = document.createElement('a');
+            a.innerHTML = '1';
+            paginate.appendChild(a);
+
+            if (product_name !== '') {
+                a.onclick = () => {
+                    this.loadProductsByName(product_name, 1);
+                };
+            } else if (category !== '') {
+                a.onclick = () => {
+                    this.loadProductsByCategory(category, 1);
+                };
+            } else {
+                a.onclick = () => {
+                    this.loadProducts(1);
+                };
+            }
+        }
+        if (hasPreviousPage) {
+            let b = document.createElement('a');
+            b.innerHTML = `${previousPage}`;
+
+            let c = document.createElement('a');
+            c.innerHTML = `${currentPage}`;
+            c.classList = 'active';
+
+            paginate.appendChild(b);
+            paginate.appendChild(c);
+
+            if (product_name !== '') {
+                b.onclick = () => {
+                    this.loadProductsByName(product_name, previousPage);
+                };
+            } else if (category !== '') {
+                b.onclick = () => {
+                    this.loadProductsByCategory(category, previousPage);
+                };
+            } else {
+                b.onclick = () => {
+                    this.loadProducts(previousPage);
+                };
+            }
+        }
+        if (hasNextPage) {
+            let d = document.createElement('a');
+            d.innerHTML = `${nextPage}`;
+            paginate.appendChild(d);
+
+            if (product_name !== '') {
+                d.onclick = () => {
+                    this.loadProductsByName(product_name, nextPage);
+                };
+            } else if (category !== '') {
+                d.onclick = () => {
+                    this.loadProductsByCategory(category, nextPage);
+                };
+            } else {
+                d.onclick = () => {
+                    this.loadProducts(nextPage);
+                };
+            }
+        }
+        if (lastPage !== currentPage && nextPage !== lastPage) {
+            let e = document.createElement('a');
+            e.innerHTML = `${lastPage}`;
+            paginate.appendChild(e);
+
+            if (product_name !== '') {
+                e.onclick = () => {
+                    this.loadProductsByName(product_name, lastPage);
+                };
+            } else if (category !== '') {
+                e.onclick = () => {
+                    this.loadProductsByCategory(category, lastPage);
+                };
+            } else {
+                e.onclick = () => {
+                    this.loadProducts(lastPage);
+                };
+            }
+        }
     }
 }
 
@@ -272,10 +401,9 @@ window.addEventListener("load", () => {
     // Lista los productos buscados por nombre
     let buscar = document.querySelector('#buscar');
     let input = document.querySelector('#InputBuscar');
-    buscar.addEventListener('click', (e)=>{
+    buscar.addEventListener('click', (e) => {
         e.preventDefault();
         const product_name = input.value;
-        console.log(product_name);
         product.loadProductsByName(product_name);
     });
 });
